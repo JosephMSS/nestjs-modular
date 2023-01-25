8fbb188a764e9c590d54f01938834eb07fe7c58f
Joseph Morales Silva <josephzx1@hotmail.com>
2023-01-25 14:52:46 -0600

    Encapsular logica en los modulos

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -6,9 +6,14 @@ import { CategoriesController } from './controllers/categories.controller';
 import { ProductsService } from './services/products.service';
 import { BrandsService } from './services/brands.service';
 import { CategoriesService } from './services/categories.service';
-
+/**
+ * Cada uno de los modulos  posee un dominio de servicio
+ * Dominio: servicios que están relacionados al modulo
+ * En este caso un producto posee: marcas y categorías
+ */
 @Module({
   controllers: [ProductsController, CategoriesController, BrandsController],
   providers: [ProductsService, BrandsService, CategoriesService],
 })
 export class ProductsModule {}
+
